3e315c530b1a0c32ae270c4c7a8205c257381f00
Nikho Sagala <nkhsgl10@gmail.com>
2016-03-18 19:34:52 +0700

    + fungsionalitas

---
+++ src/projectmanagement/Programmer.java
@@ -0,0 +1,29 @@
+package projectmanagement;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Mayya
+ */
+public class Programmer extends Orang {
+
+    private String id;
+
+    public Programmer(String nama, int umur, String alamat) {
+        super(nama, umur, alamat);
+        id = "";
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+}
